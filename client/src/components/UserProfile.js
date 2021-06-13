b3af561109b37073864dacd3738bba606567cde2
Kelvin Lien <Mr.Secret0987@gmail.com>
2021-06-13 23:27:31 +0700

    feat: add UserProfile

---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,15 @@
+import {Avatar, Container, Typography} from "@material-ui/core";
+
+
+export default function UserProfile(props){
+    return(
+        <Container>
+            <Avatar alt="Profile avatar" src={"../assets/images/reactlogo192.png"}></Avatar>
+            <div>
+                <Typography variant={"h2"} component={"p"}>
+                    Name and age
+                </Typography>
+            </div>
+        </Container>
+    );
+}
