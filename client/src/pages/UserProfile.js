d24fa46c6c66d636bcd1a3e00e99c140287494b8
Kelvin Lien <Mr.Secret0987@gmail.com>
2021-06-14 00:21:52 +0700

    feat: add Dashboard and required props to UserProfile

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,9 +1,26 @@
 import UserInfo from "../components/UserInfo";
 import ProductLine from "../components/ProductLine";
+import Dashboard from "../components/Dashboard";
+import HomeIcon from '@material-ui/icons/Home';
+import {ExitToApp} from "@material-ui/icons";
+import history from "../utils/History";
+
+function handleOpenHomePage(){
+    history.push("/home");
+}
+
+function handleSignOut(){
+    history.push("/");
+}
 
 export default function UserProfile(props){
     return(
         <fragment>
+            <Dashboard
+                leftButtonIcon={<HomeIcon/>}
+                rightButtonIcon={<ExitToApp/>}
+                handleLeftButtonClick={handleOpenHomePage}
+                handleRightButtonClick={handleSignOut}/>
             <UserInfo></UserInfo>
             <ProductLine></ProductLine>
         </fragment>
