3494989f6b80221e49f8e8e6b793f718f6db6e71
Kelvin Lien <Mr.Secret0987@gmail.com>
2021-07-13 00:32:54 +0700

    feat: showing image from api, limit title line to 2, limit subtitle line to 1

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,18 +1,40 @@
 import React from "react"
 import {Card, Typography} from "@material-ui/core";
 import altImg from "../assets/images/reactlogo192.png";
+import {makeStyles} from "@material-ui/core/styles";
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: "200px", height: "320px"
+    },
+    image: {
+        height: "220px",
+        width: "200px"
+    },
+    title: {
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        display: "-webkit-box",
+        "-webkit-line-clamp": 2,
+        "-webkit-box-orient": "vertical"
+    },
+    subtitle: {
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+    }
+}))
 export default function ProductCard(props) {
+    const classes = useStyles();
     return (
-        <Card raised={true} style={{width: "200px", height: "320px"}}>
-            <img src={props.imageUrl} alt={altImg} className={{  height: "50px",
-                width: "50px"}}/>
-            <Typography variant={"h5"} component={"h2"} color={"black"} align={"center"}>
+        <Card raised={true} className={classes.root}>
+            <img src={props.imageUrl} alt={altImg} className={classes.image}/>
+            <Typography variant={"h5"} component={"h1"} color={"black"} align={"center"} className={classes.title}>
                 {
                     props.title
                 }
             </Typography>
-            <Typography variant={"p"} component={"p"} color={"black"} align={"center"}>
+            <Typography variant={"h6"} component={"h2"} color={"black"} align={"center"}>
                 {
                     props.subtitle
                 }
--- client/src/components/ProductLine.js
+++ client/src/components/ProductLine.js
@@ -3,6 +3,7 @@ import {Grid, Typography, Paper} from "@material-ui/core";
 import CustomizedCard from "./CustomizedCard";
 import ProductCard from "./ProductCard";
 import {makeStyles} from "@material-ui/core/styles";
+import axios from "axios";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,21 +23,49 @@ export default function ProductLine(props) {
 
     function generateLineOfProducts(keys, products) {
         let data = [];
-        let imageString = true;
-        for (let i = 0; i < products.length; i++) {
-            data.push(
-                <Grid item key={i}>
-                    <CustomizedCard
-                        cardContent={<ProductCard
-                            imageUrl={products[i][keys.imageUrl]}
-                            key={products[i][keys.id]}
-                            title={products[i][keys.title]}
-                            subtitle={products[i][keys.subtitle]}
-                        ></ProductCard>}
-                        cardAction={""}
-                    />
-                </Grid>
-            );
+        if (products.length > 0)
+        {
+            for (let i = 0; i < products.length; i++) {
+                let _imageUrl, _key, _title, _subtitle;
+                _imageUrl = _key = _title = _subtitle  = products[i] ;
+                let imageUrlArray = keys.imageUrl.split(".");
+                let keyArray = keys.id.split(".");
+                let titleArray = keys.title.split(".");
+                let subtitleArray = keys.subtitle.split(".");
+                for (let val of imageUrlArray)
+                {
+                    _imageUrl = _imageUrl[val];
+                    if (val === "image_urls")
+                    {
+                        _imageUrl = JSON.parse(_imageUrl)[0];
+                    }
+                }
+                for (let val of keyArray)
+                {
+                    _key = _key[val];
+                }
+                for (let val of titleArray)
+                {
+                    _title = _title[val];
+                }
+                for (let val of subtitleArray)
+                {
+                    _subtitle = _subtitle[val];
+                }
+                data.push(
+                    <Grid item key={i}>
+                        <CustomizedCard
+                            cardContent={<ProductCard
+                                imageUrl={_imageUrl}
+                                key={_key}
+                                title={_title}
+                                subtitle={_subtitle}
+                            ></ProductCard>}
+                            cardAction={""}
+                        />
+                    </Grid>
+                );
+            }
         }
         return data;
 
