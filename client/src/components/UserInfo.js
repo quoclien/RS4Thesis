a14f1647ed751055faab3a9578676d272eafc155
Kelvin Lien <Mr.Secret0987@gmail.com>
2021-07-20 11:27:29 +0700

    feat: use paper for UserInfo

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -1,22 +1,36 @@
-import {Avatar, Typography} from "@material-ui/core";
+import {Avatar, Paper, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyle = makeStyles((theme) => ({
+    root:{
+        marginLeft: "35%",
+        marginRight: "35%"
+    },
     flexParent: {
         display: 'flex',
-        alignItems: 'center',
         justifyContent: 'center',
         flexFlow: "column wrap",
         marginTop: "80px",
-        marginBottom: "10px"
+        marginBottom: "10px",
+        marginRight: "20px",
+        marginLeft: "20px",
     },
-    flexChild: {},
+    flexChild: {
+        marginTop: "5px",
+        marginBottom: "5px"
+    },
+    flexAvatar: {
+        margin: "auto"
+    },
     bigAvatar: {
-        width: theme.spacing(7),
-        height: theme.spacing(7)
+        width: theme.spacing(9),
+        height: theme.spacing(9)
     },
-    name:{
-
+    info:{
+        fontWeight: 400,
+        fontSize: "15px",
+        lineHeight: "15px",
+        color: "#333333"
     }
 }))
 
@@ -24,15 +38,32 @@ export default function UserInfo(props) {
     const classes = useStyle();
     const userInfo = props.userInfo;
     return (
-        <div className={classes.flexParent}>
-            <div className={classes.flexChild}>
-                <Avatar alt={"Avatar of " + userInfo.username} src={userInfo.avatar} className={classes.bigAvatar}/>
+        <Paper variant="outlined" className={classes.root}>
+            <div className={classes.flexParent}>
+                <div className={classes.flexAvatar}>
+                    <Avatar alt={"Avatar of " + userInfo.username} src={userInfo.avatar} className={classes.bigAvatar}/>
+                </div>
+                <div className={classes.flexChild}>
+                    <Typography variant={"h6"} component={"p"} className={classes.info}>
+                        {"Name: " + userInfo.name}
+                    </Typography>
+                </div>
+                <div className={classes.flexChild}>
+                    <Typography variant={"h6"} component={"p"} className={classes.info}>
+                        {"Email: " + userInfo.email}
+                    </Typography>
+                </div>
+                <div className={classes.flexChild}>
+                    <Typography variant={"h6"} component={"p"} className={classes.info}>
+                        {"Age: " + userInfo.age}
+                    </Typography>
+                </div>
+                <div className={classes.flexChild}>
+                    <Typography variant={"h6"} component={"p"} className={classes.info}>
+                        {"Gender (M/F): " + userInfo.gender}
+                    </Typography>
+                </div>
             </div>
-            <div className={classes.flexChild}>
-                <Typography variant={"h6"} component={"p"} className={classes.name}>
-                    {userInfo.name}
-                </Typography>
-            </div>
-        </div>
+        </Paper>
     );
 }
